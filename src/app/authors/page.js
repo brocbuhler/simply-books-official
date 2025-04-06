d24f0da6d146118d71ba009ffb884e4ede5006e4
Broc Buhler <brocbuhler@gmail.com>
2025-04-06 06:59:35 -0700

    author route rendering is working

--- src/app/authors/page.js
+++ src/app/authors/page.js
@@ -1,5 +1,33 @@
-import React from 'react';
+'use client';
 
-export default function Author() {
-  return <div>author page</div>;
+import React, { useEffect, useState } from 'react';
+import { useAuth } from '../../utils/context/authContext';
+import { getAuthors } from '../../api/authorData';
+import AuthorCard from '../../components/AuthorCard';
+
+export default function ViewAllAuthors() {
+  const [authors, setAuthors] = useState([]);
+
+  // grab firebaseKey from url
+  const { user } = useAuth();
+
+  // make call to API layer to get the data
+  const getAllTheAuthors = () => {
+    getAuthors(user.uid).then(setAuthors);
+  };
+
+  useEffect(() => {
+    getAllTheAuthors();
+  }, []);
+
+  console.log(authors);
+  return (
+    <div className="text-center my-4">
+      <div className="d-flex flex-wrap">
+        {authors.map((author) => (
+          <AuthorCard key={author.firebaseKey} authObj={author} onUpdate={getAllTheAuthors} />
+        ))}
+      </div>
+    </div>
+  );
 }
---
+++ src/components/AuthorCard.js
@@ -0,0 +1,54 @@
+'use client';
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Link from 'next/link';
+import { deleteSingleAuthor } from '../api/authorData';
+
+export default function AuthorCard({ authObj, onUpdate }) {
+  // FOR DELETE, WE NEED TO REMOVE THE BOOK AND HAVE THE VIEW RERENDER,
+  // SO WE PASS THE FUNCTION FROM THE PARENT THAT GETS THE AUTHORS
+  const deleteThisAuthor = () => {
+    if (window.confirm(`Delete ${authObj.last_name}?`)) {
+      deleteSingleAuthor(authObj.firebaseKey).then(() => onUpdate());
+    }
+  };
+
+  return (
+    <Card style={{ width: '18rem', margin: '10px' }}>
+      <Card.Body>
+        <Card.Title>
+          {authObj.first_name} {authObj.last_name}
+        </Card.Title>
+        <p className="card-text bold">{authObj.email}</p>
+        <p className="card-text bold">Is favorite? : {authObj.favorite}</p>
+        {/* DYNAMIC LINK TO VIEW THE AUTHOR DETAILS  */}
+        <Link href={`/book/${authObj.firebaseKey}`} passHref>
+          <Button variant="primary" className="m-2">
+            VIEW
+          </Button>
+        </Link>
+        {/* DYNAMIC LINK TO EDIT THE AUTHOR DETAILS  */}
+        <Link href={`/book/edit/${authObj.firebaseKey}`} passHref>
+          <Button variant="info">EDIT</Button>
+        </Link>
+        <Button variant="danger" onClick={deleteThisAuthor} className="m-2">
+          DELETE
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+AuthorCard.propTypes = {
+  authObj: PropTypes.shape({
+    first_name: PropTypes.string,
+    last_name: PropTypes.string,
+    favorite: PropTypes.bool,
+    email: PropTypes.string,
+    firebaseKey: PropTypes.string,
+  }).isRequired,
+  onUpdate: PropTypes.func.isRequired,
+};
